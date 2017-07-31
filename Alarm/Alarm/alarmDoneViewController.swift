25f4c52e6020ed6d0e9c2e4085529b507c06e443
F-guy <zhangxufei05@hotmail.com>
2017-07-31 15:23:45 +0800

    Changed t0 CoreData Project

---
+++ Alarm/Alarm/alarmDoneViewController.swift
@@ -0,0 +1,18 @@
+//
+//  alarmDoneViewController.swift
+//  Alarm
+//
+//  Created by chuangke-8 on 7/31/17.
+//  Copyright © 2017 Make School. All rights reserved.
+//
+
+import UIKit
+
+class alarmDoneViewController: UIViewController {
+    
+    @IBOutlet weak var whyAnswer: UITextField!
+    @IBOutlet weak var mathAnswer: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Alarm/Alarm/alarmsTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  alarmsTableViewController.swift
+//  Alarm
+//
+//  Created by chuangke-8 on 7/31/17.
+//  Copyright © 2017 Make School. All rights reserved.
+//
+
+import UIKit
+
+class alarmsTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
