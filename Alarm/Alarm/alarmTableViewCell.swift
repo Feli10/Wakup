98be3abe9c18bcfbdab39c2373aafcaa059da225
F-guy <zhangxufei05@hotmail.com>
2017-07-31 17:07:26 +0800

    Hackathon Day 1! Coredata Finally Works!

---
+++ Alarm/Alarm/alarmTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  alarmTableViewCell.swift
+//  Alarm
+//
+//  Created by chuangke-8 on 7/31/17.
+//  Copyright © 2017 Make School. All rights reserved.
+//
+
+import UIKit
+
+class alarmTableViewCell: UITableViewCell {
+    @IBOutlet weak var alarmTitle: UILabel!
+    
+}
--- Alarm/Alarm/alarmsTableViewController.swift
+++ Alarm/Alarm/alarmsTableViewController.swift
@@ -10,6 +10,32 @@ import UIKit
 
 class alarmsTableViewController: UITableViewController {
     
+    var alarms = [Alarm]() {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    // 1
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return alarms.count
+    }
+    // 2
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "alarmTableViewCell", for: indexPath) as! alarmTableViewCell
+        
+        // 1
+        let row = indexPath.row
+        
+        // 2
+        let alarm = alarms[row]
+        
+        // 3
+        cell.alarmTitle.text = alarm.title
+        
+        
+        return cell
+    }
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
